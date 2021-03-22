7970e3f59477d59bd32f7ed4820e7cfc06fbcc96
yinxing.ws <yinxing.ws@alibaba-inc.com>
2021-03-22 14:31:10 +0800

    增加oasis demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react'
+import { WebCanvas, Engine, WebGLRenderer, Camera, Vector4 } from 'oasis-engine'
+import './App.css'
 
 function App() {
+  useEffect(() => {
+    const canvas = document.getElementById('canvas')
+    const webCanvas = new WebCanvas(canvas)
+
+    let engine = new Engine(webCanvas, new WebGLRenderer())
+
+    const scene = engine.sceneManager.activeScene
+    const rootEntity = scene.createRootEntity('root')
+
+    const cameraNode = rootEntity.createChild('myCamera')
+    const camera = cameraNode.addComponent(Camera)
+    camera.backgroundColor = new Vector4(1, 1, 1, 0)
+
+    engine.run()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: '500px', height: '500px', backgroundColor: 'gray' }}>
+      <canvas
+        id='canvas'
+        width='500'
+        height='500'
+        style={{ width: '500px', height: '500px' }}
+      ></canvas>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
